95a29263b7861f5a6d07c9d0ac617b94ea615bb6
omarshah0 <oemyoem55@gmail.com>
2024-01-11 13:00:08 +0500

    Fixed issues

--- config/config.go
+++ config/config.go
@@ -1,13 +1,24 @@
 package config
 
+import (
+	"log"
+	"os"
+)
+
 type Config struct {
 	Port        string
 	DatabaseUrl string
 }
 
 func NewConfig() *Config {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
 	return &Config{
-		Port:        ":8080",
+		Port:        ":" + port,
 		DatabaseUrl: "postgres://postgres:Unclesnoopdog@69@localhost:5432/go_clean",
 	}
 }
