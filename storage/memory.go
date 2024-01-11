aa1f0725f1072ac104795ee5005ed61f836ef2f9
omarshah0 <oemyoem55@gmail.com>
2024-01-11 12:42:12 +0500

    New storage for testing

---
+++ storage/memory.go
@@ -0,0 +1,16 @@
+package storage
+
+import (
+	"github.com/omarshah0/go-clean-architecture/config"
+	"github.com/omarshah0/go-clean-architecture/types"
+)
+
+type MemoryStore struct{}
+
+func NewMemoryStorage(config *config.Config) (*MemoryStore, error) {
+	return &MemoryStore{}, nil
+}
+
+func (s *MemoryStore) GetAllUsers() ([]*types.User, error) {
+	return []*types.User{}, nil
+}
